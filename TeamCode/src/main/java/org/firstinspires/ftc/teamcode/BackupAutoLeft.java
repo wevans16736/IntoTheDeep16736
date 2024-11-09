b894050d5d3ce6bae9b41316d2bee23c0a8eca47
22savagelol <jkjgoldfish@icloud.com>
2024-11-09 09:58:33 -0600

    add BackupAutoLeft.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackupAutoLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackupAutoLeft.java
@@ -1,33 +1,25 @@
-package org.firstinspires.ftc.teamcode.Autonomus;
+package org.firstinspires.ftc.teamcode;
 
 // RR-specific imports
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
 
-//Non-RR imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-//Team code imports
-import org.firstinspires.ftc.teamcode.PinpointDrive;
 import org.firstinspires.ftc.teamcode.robotverticalslides.DriveActions;
+import org.firstinspires.ftc.teamcode.robotverticalslides.HorizontalSlide.HorizontalIntakeActions;
 import org.firstinspires.ftc.teamcode.robotverticalslides.HorizontalSlide.HorizontalSlideActions;
 import org.firstinspires.ftc.teamcode.robotverticalslides.HorizontalSlide.HorizontalWristActions;
-import org.firstinspires.ftc.teamcode.robotverticalslides.HorizontalSlide.HorizontalIntakeActions;
 import org.firstinspires.ftc.teamcode.robotverticalslides.VerticalSlide.VerticalGrabberActions;
 import org.firstinspires.ftc.teamcode.robotverticalslides.VerticalSlide.VerticalSlideActions;
 import org.firstinspires.ftc.teamcode.robotverticalslides.VerticalSlide.VerticalWristActions;
-//import org.firstinspires.ftc.teamcode.Autonomus.Configuration;
 
 
 @Config
-@Autonomous(name = "MainAuto", group = "Autonomus")
-public class MainAutonomus extends LinearOpMode {
+@Autonomous(name = "Backup Auto Left", group = "Autonomus")
+public class BackupAutoLeft extends LinearOpMode{
 
     private DriveActions driveActions = null;
     private HorizontalSlideActions horizontalSlide = null;
@@ -37,11 +29,8 @@ public class MainAutonomus extends LinearOpMode {
     private VerticalWristActions verticalWrist = null;
     private VerticalGrabberActions verticalGrabber = null;
 
-    //Configuration config = new Configuration;
-
     @Override
     public void runOpMode() throws InterruptedException {
-        //set up all the hardware map for the robot and instantiate them as a object
         driveActions = new DriveActions(telemetry, hardwareMap);
         horizontalSlide = new HorizontalSlideActions(hardwareMap, telemetry);
         horizontalWrist = new HorizontalWristActions(telemetry, hardwareMap);
@@ -55,34 +44,34 @@ public class MainAutonomus extends LinearOpMode {
         Pose2d initialPose = new Pose2d(0,0, Math.toRadians(90));
         Vector2d vector2d = new Vector2d(0,0);
         PinpointDrive drive = new PinpointDrive(hardwareMap, initialPose);
-
-        //trajectory from initial spot moving to blue parking spot
-        //todo find the correct blue park position and put it below
-        
-        TrajectoryActionBuilder parkBlue = drive.actionBuilder(initialPose)
-                .splineTo(new Vector2d(20,30), Math.PI/2);
-
-        //trajectory from initial spot moving to red parking spot
-        TrajectoryActionBuilder parkRed = drive.actionBuilder(initialPose)
-                .splineTo(new Vector2d(20,30), Math.PI/2);
-
-
-        //wait for the start button to be press
+        telemetry.update();
         waitForStart();
-        //if stop button is press, automatically stop
-        if (isStopRequested()) return;
-        //choosing which trajectory to take, (so far only one is made)
-        Action trajectoryActionChosen;
-            trajectoryActionChosen = parkBlue.build();
+        if (opModeIsActive()) {
+            //Putting initial sample on rung
+            //Flip wrist out
+            verticalWrist.flipping(true);
+            //set slide up
+            verticalSlide.setSlidePosition(-540, 2500);
+            //drive forward 30 inches
 
-        //run the chosen action blocking
-        Actions.runBlocking(
-            new SequentialAction(
-                trajectoryActionChosen)
+            driveActions.drive(0, 0.2, 0);
+            sleep(3800);
+            driveActions.drive(0, 0, 0);
+            verticalGrabber.open();
+            sleep(500);
+            //drive back 5 inches
+            driveActions.drive(0, -0.5, 0);
+            sleep(600);
+            driveActions.drive(0, 0, 0);
 
-        );
+            //reset outtake arm
+            verticalGrabber.close();
+            sleep(500);
+            verticalWrist.flipping(false);
+            verticalWrist.flipping(true);
+            verticalSlide.setSlidePosition(0, 2000);
+            sleep(1000);
+
+        }
     }
 }
-
-
-
