8edbe59e27eaf7dc036d8145d93e068922b9e174
22savagelol <jkjgoldfish@icloud.com>
2025-02-10 12:00:01 -0600

    make it look simpler

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric/Attachment.java
@@ -0,0 +1,264 @@
+package org.firstinspires.ftc.teamcode.FieldCentric;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+
+import org.firstinspires.ftc.teamcode.Configuration.secondRobot.ConfigurationSecondRobot;
+import org.firstinspires.ftc.teamcode.Configuration.secondRobot.HorizontalGrabberRR;
+import org.firstinspires.ftc.teamcode.Configuration.secondRobot.HorizontalRollRR;
+import org.firstinspires.ftc.teamcode.Configuration.secondRobot.HorizontalSlideRR;
+import org.firstinspires.ftc.teamcode.Configuration.secondRobot.HorizontalWristRR;
+import org.firstinspires.ftc.teamcode.Configuration.secondRobot.VerticalGrabberRR;
+import org.firstinspires.ftc.teamcode.Configuration.secondRobot.VerticalHangerRR;
+import org.firstinspires.ftc.teamcode.Configuration.secondRobot.VerticalSlideRR;
+import org.firstinspires.ftc.teamcode.Configuration.secondRobot.VerticalWristRR;
+
+import java.util.List;
+
+public class Attachment {
+    VerticalSlideRR verticalSlide; VerticalWristRR verticalWrist;
+    VerticalGrabberRR verticalGrabber; HorizontalSlideRR horizontalSlide;
+    HorizontalRollRR horizontalRoll; HorizontalGrabberRR horizontalGrabber;
+    HorizontalWristRR horizontalWrist; VerticalHangerRR verticalHanger;
+    double currTime; double desiredLoopms = 250; List<Action> runningActions;
+
+    public Attachment(VerticalSlideRR verticalSlide, VerticalWristRR verticalWrist,
+                      VerticalGrabberRR verticalGrabber, HorizontalSlideRR horizontalSlide,
+                      HorizontalRollRR horizontalRoll, HorizontalGrabberRR horizontalGrabber,
+                      HorizontalWristRR horizontalWrist, VerticalHangerRR verticalHanger, List<Action> runningActions){
+        this.verticalSlide = verticalSlide;
+        this.verticalWrist = verticalWrist;
+        this.verticalGrabber = verticalGrabber;
+        this.horizontalSlide = horizontalSlide;
+        this.horizontalRoll = horizontalRoll;
+        this.horizontalGrabber = horizontalGrabber;
+        this.horizontalWrist = horizontalWrist;
+        this.verticalHanger = verticalHanger;
+
+        this.runningActions = runningActions;
+        currTime =System.currentTimeMillis();
+    }
+    boolean wasRB = false; double prevTimeRB = 0.0; double LoopTimeRB;
+    public void verticalGrabber(boolean right_bumper) {
+        //vertical grabber
+        LoopTimeRB= currTime - prevTimeRB;
+        //update need from gamepads
+        if(right_bumper){
+            if (LoopTimeRB >= desiredLoopms) {
+                if (!wasRB) {
+                    runningActions.add(new SequentialAction(
+                            new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalOpen))
+                    ));
+                } else {
+                    if (wasRB) {
+                        runningActions.add(new SequentialAction(
+                                new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalClose))
+                        ));
+                    }
+                }
+                prevTimeRB = currTime;
+                wasRB = !wasRB;
+            }
+        }
+    }
+    boolean wasTriangle = false; double prevTimeTriangle = 0.0; double loopTimeTriangle;
+    public void horizontalSlide(boolean triangle) {
+        loopTimeTriangle = currTime - prevTimeTriangle;
+        if(triangle) {
+            if (loopTimeTriangle >= desiredLoopms) {
+                if (!wasTriangle) {
+                    runningActions.add(new SequentialAction(
+                            new InstantAction(() -> horizontalSlide.setPose(ConfigurationSecondRobot.horizontalSlideExtend)),
+                            new SleepAction(.5),
+                            new InstantAction(() -> horizontalWrist.setPose(ConfigurationSecondRobot.horizontalWristIntake)),
+                            new InstantAction(() -> horizontalRoll.setPose(ConfigurationSecondRobot.slant)),
+                            new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalOpen)),
+                            new InstantAction(() -> horizontalGrabber.setPose(ConfigurationSecondRobot.horizontalGrabberOpen))
+                    ));
+                } else {
+                    if (wasTriangle) {
+                        runningActions.add(new SequentialAction(
+                                new InstantAction(() -> horizontalWrist.setPose(ConfigurationSecondRobot.horizontalWristHover)),
+                                new InstantAction(() -> horizontalRoll.setPose(ConfigurationSecondRobot.flat)),
+                                new InstantAction(() -> horizontalSlide.setPose(ConfigurationSecondRobot.horizontalSlideRetract)),
+                                new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalOpen)),
+                                new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristIntake)),
+                                new SleepAction(ConfigurationSecondRobot.horizontalSlideTime / 1000),
+                                new InstantAction(() -> horizontalWrist.setPose(ConfigurationSecondRobot.horizontalWristTransfer)),
+                                new SleepAction(.5),
+                                new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalClose)),
+                                new SleepAction(ConfigurationSecondRobot.verticalCloseTime / 1000),
+                                new InstantAction(() -> horizontalGrabber.setPose(ConfigurationSecondRobot.horizontalGrabberOpen))
+                        ));
+                    }
+                }
+                prevTimeTriangle = currTime;
+                wasTriangle = !wasTriangle;
+            }
+        }
+    }
+
+    double loopTimeRT; boolean wasRT = false; double prevTimeRT = 0.0;
+    public void horizontalGrabber(double right_trigger) {
+        loopTimeRT = currTime - prevTimeRT;
+        //update input from gamepad
+        if (right_trigger >= .5) {
+            if (loopTimeRT >= desiredLoopms) {
+                if (!wasRT) {
+                    runningActions.add(new SequentialAction(
+                            new InstantAction(() -> horizontalGrabber.setPose(ConfigurationSecondRobot.horizontalGrabberClose))
+                    ));
+                } else {
+                    if (wasRT) {
+                        runningActions.add(new SequentialAction(
+                                new InstantAction(() -> horizontalGrabber.setPose(ConfigurationSecondRobot.horizontalGrabberOpen))
+                        ));
+                    }
+                }
+                prevTimeRT = currTime;
+                wasRT = !wasRT;
+            }
+        }
+    }
+    double wasLeft; double loopTimeLeft; double prevTimeLeft = 0.0;
+    public void verticalSlide(double left_trigger, boolean left_bumper){
+        //vertical preset
+        loopTimeLeft = currTime - prevTimeLeft;
+        //update input from gamepad
+        if (left_trigger >= .5 || left_bumper) {
+                if (loopTimeLeft >= desiredLoopms) {
+                    if (left_trigger >= .5) {
+                        wasLeft -= 1;
+                        if (wasLeft <= 0) {
+                            wasLeft = 0;
+                        }
+                    }
+                    if (left_bumper) {
+                        wasLeft += 1;
+                        if (wasLeft >= 3) {
+                            wasLeft = 3;
+                        }
+                    }
+                    if (wasLeft == 0) {
+                        runningActions.add(new SequentialAction(
+                                new InstantAction(() -> verticalSlide.setPose(ConfigurationSecondRobot.bottom)),
+                                new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristIntake)),
+                                new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalOpen))
+                        ));
+                    }
+                    if (wasLeft == 1) {
+                        runningActions.add(new SequentialAction(
+                                new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristWall)),
+                                new InstantAction(() -> verticalSlide.setPose(ConfigurationSecondRobot.bottom)),
+                                new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalClose))
+                        ));
+                    }
+                    if (wasLeft == 2) {
+                        runningActions.add(new SequentialAction(
+                                new InstantAction(() -> verticalSlide.setPose(ConfigurationSecondRobot.highBar)),
+                                new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristBar))
+                        ));
+                    }
+                    if (wasLeft == 3) {
+                        runningActions.add(new SequentialAction(
+                                new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristBasket)),
+                                new InstantAction(() -> verticalSlide.setPose(ConfigurationSecondRobot.topBasket))
+                        ));
+                    }
+                    prevTimeLeft = currTime;
+                }
+
+        }
+    }
+    double loopTimeCircle; double prevTimeCircle = 0.0; boolean wasCircle = false;
+    public void transferWrist(boolean circle){
+        //transfer wrist
+        loopTimeCircle = currTime - prevTimeCircle;
+        //update input from gamepad
+        if (circle) {
+            if (loopTimeCircle <= desiredLoopms) {
+                if (wasCircle) {
+                    runningActions.add(new SequentialAction(
+                            new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristIntake))
+                    ));
+                } else {
+                    if (!wasCircle) {
+                        runningActions.add(new SequentialAction(
+                                new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristWall)),
+                                new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalOpen))
+                        ));
+                    }
+                }
+                prevTimeCircle = currTime;
+                wasCircle = !wasCircle;
+            }
+        }
+    }
+    double loopTimeDPU; double prevTimeDPU = 0.0; boolean wasDPU;
+    public void hook(boolean dpad_up){
+        //hook
+        loopTimeDPU = currTime - prevTimeDPU;
+        //update input from gamepad
+        if (dpad_up) {
+            if (!wasCross) {
+                if (loopTimeDPU >= desiredLoopms) {
+                    if (wasDPU) {
+                        runningActions.add(new SequentialAction(
+                                new InstantAction(() -> verticalHanger.setPose(ConfigurationSecondRobot.verticalHangIn))
+                        ));
+                    } else {
+                        if (!wasDPU) {
+                            runningActions.add(new SequentialAction(
+                                    new InstantAction(() -> verticalHanger.setPose(ConfigurationSecondRobot.verticalHangOut))
+                            ));
+                        }
+                    }
+                    prevTimeDPU = currTime;
+                    wasDPU = !wasDPU;
+                }
+            }
+        }
+    }
+    double loopTimeSquare; double prevTimeSquare = 0.0; boolean wasSquare;
+    public boolean percise(boolean square){
+        loopTimeSquare = currTime - prevTimeSquare;
+        //update input from gamepad
+        if (square) {
+            if (loopTimeSquare >= desiredLoopms) {
+                prevTimeSquare = currTime;
+                wasSquare = !wasSquare;
+            }
+        }
+        return wasSquare;
+    }
+    double loopTimeCross; double prevTimeCross = 0.0; boolean wasCross = false; int slidePose;
+    double loopTimedpad; double prevTimedpad = 0.0;
+    public void verticalOverride(boolean cross, boolean dpad_up, boolean dpad_down){
+        //manual override for vertical slide
+        loopTimeCross = currTime - prevTimeCross;
+        loopTimedpad = currTime - prevTimedpad;
+        //update input from gamepad
+        if (cross) {
+            if(loopTimeCross >= desiredLoopms){
+                if(wasCross){
+                    slidePose = verticalSlide.returnPose();
+                   if(loopTimedpad >= desiredLoopms){
+                       if(dpad_up){
+                           slidePose += 50;
+                           verticalSlide.setPose(slidePose);
+                       }
+                       if(dpad_down){
+                           slidePose -= 50;
+                           verticalSlide.setPose(slidePose);
+                       }
+                       prevTimedpad = currTime;
+                   }
+                }
+                prevTimeCross = currTime;
+                wasCross = !wasCross;
+            }
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric/Telop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric/Telop.java
@@ -42,6 +42,7 @@ public class Telop extends OpMode {
     HorizontalWristRR horizontalWrist;
     DriveTrain driveTrain;
     Pose2d currentPose;
+    Attachment attachment;
 
     @Override
     public void init() {
@@ -59,7 +60,9 @@ public class Telop extends OpMode {
         horizontalGrabber = new HorizontalGrabberRR(hardwareMap);
         horizontalWrist = new HorizontalWristRR(hardwareMap);
         verticalHanger = new VerticalHangerRR(hardwareMap);
-
+        attachment = new Attachment(verticalSlide, verticalWrist, verticalGrabber,
+                horizontalSlide, horizontalRoll, horizontalGrabber, horizontalWrist, verticalHanger,
+                runningActions);
 
         //todo localization after auto?
         pose = new Pose2d(0, 0, Math.toRadians(90));
@@ -79,242 +82,32 @@ public class Telop extends OpMode {
         }
         GlobalVarable.autoStarted = false;
     }
-
-    //time
-    double startTime = 0;
-    double currTime = 0;
-    double prevTimeRB = 0.0;
-    double loopTimeRB = 0.0;
-    boolean wasRB = false;
-    double prevTimeTriangle = 0.0;
-    double loopTimeTriangle = 0.0;
-    boolean wasTriangle = false;
-    double prevTimeRT = 0.0;
-    double loopTimeRT = 0.0;
-    boolean wasRT;
-    double prevTimeLeft = 0.0;
-    double loopTimeLeft = 0.0;
-    int wasLeft = 0;
-    double prevTimeCircle = 0.0;
-    double loopTimeCircle = 0.0;
-    boolean wasCircle = false;
-    double prevTimeDPU = 0.0;
-    double loopTimeDPU = 0.0;
-    boolean wasDPU = false;
-    double prevTimeSquare = 0.0;
-    double loopTimeSquare = 0.0;
-    boolean wasSquare = false;
-    double prevTimeCross = 0.0;
-    double loopTimeCross = 0.0;
-    boolean wasCross = false;
-    int slidePose = 0;
-    double percise = 1;
-    //how long is the delay between press
-    double desiredLoopms = 250.0;
-    double RTms = 500;
-
+    boolean percise = false;
     @Override
     public void loop() {
         TelemetryPacket packet = new TelemetryPacket();
-        //vertical grabber
-        currTime = System.currentTimeMillis();
-        loopTimeRB = currTime - prevTimeRB;
-        //update need from gamepads
-        if (gamepad1.right_bumper) {
-            if (loopTimeRB >= desiredLoopms) {
-                if (!wasRB) {
-                    runningActions.add(new SequentialAction(
-                            new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalOpen))
-                    ));
-                } else {
-                    if (wasRB) {
-                        runningActions.add(new SequentialAction(
-                                new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalClose))
-                        ));
-                    }
-                }
-                prevTimeRB = currTime;
-                wasRB = !wasRB;
-            }
-        }
+        //verticalGrabber
+        attachment.verticalGrabber(gamepad1.right_bumper);
         //horizontal slide
-        loopTimeTriangle = currTime - prevTimeTriangle;
-        //update need from gamepads
-        if (gamepad1.triangle) {
-            if (loopTimeTriangle >= desiredLoopms) {
-                if (!wasTriangle) {
-                    runningActions.add(new SequentialAction(
-                            new InstantAction(() -> horizontalSlide.setPose(ConfigurationSecondRobot.horizontalSlideExtend)),
-                            new SleepAction(.5),
-                            new InstantAction(() -> horizontalWrist.setPose(ConfigurationSecondRobot.horizontalWristIntake)),
-                            new InstantAction(() -> horizontalRoll.setPose(ConfigurationSecondRobot.slant)),
-                            new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalOpen)),
-                            new InstantAction(() -> horizontalGrabber.setPose(ConfigurationSecondRobot.horizontalGrabberOpen))
-                    ));
-                } else {
-                    if (wasTriangle) {
-                        runningActions.add(new SequentialAction(
-                                new InstantAction(() -> horizontalWrist.setPose(ConfigurationSecondRobot.horizontalWristHover)),
-                                new InstantAction(() -> horizontalRoll.setPose(ConfigurationSecondRobot.flat)),
-                                new InstantAction(() -> horizontalSlide.setPose(ConfigurationSecondRobot.horizontalSlideRetract)),
-                                new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalOpen)),
-                                new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristIntake)),
-                                new SleepAction(ConfigurationSecondRobot.horizontalSlideTime/1000),
-                                new InstantAction(() -> horizontalWrist.setPose(ConfigurationSecondRobot.horizontalWristTransfer)),
-                                new SleepAction(.5),
-                                new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalClose)),
-                                new SleepAction(ConfigurationSecondRobot.verticalCloseTime /1000),
-                                new InstantAction(() -> horizontalGrabber.setPose(ConfigurationSecondRobot.horizontalGrabberOpen))
-                        ));
-                    }
-                }
-                prevTimeTriangle = currTime;
-                wasTriangle = !wasTriangle;
-            }
-        }
+        attachment.horizontalSlide(gamepad1.triangle);
         //horizontal grabber
-        loopTimeRT = currTime - prevTimeRT;
-        //update input from gamepad
-        if (gamepad1.right_trigger >= .5) {
-            if (loopTimeRT >= RTms) {
-                if (!wasRT) {
-                    runningActions.add(new SequentialAction(
-                            new InstantAction(() -> horizontalGrabber.setPose(ConfigurationSecondRobot.horizontalGrabberClose))
-                    ));
-                } else {
-                    if (wasRT) {
-                        runningActions.add(new SequentialAction(
-                                new InstantAction(() -> horizontalGrabber.setPose(ConfigurationSecondRobot.horizontalGrabberOpen))
-                        ));
-                    }
-                }
-                prevTimeRT = currTime;
-                wasRT = !wasRT;
-            }
-        }
-        //vertical preset
-        loopTimeLeft = currTime - prevTimeLeft;
-        //update input from gamepad
-        if (gamepad1.left_trigger >= .5 || gamepad1.left_bumper) {
-            if (loopTimeLeft >= desiredLoopms) {
-                if (gamepad1.left_trigger >= .5) {
-                    wasLeft -= 1;
-                    if (wasLeft <= 0) {
-                        wasLeft = 0;
-                    }
-                }
-                if (gamepad1.left_bumper) {
-                    wasLeft += 1;
-                    if (wasLeft >= 3) {
-                        wasLeft = 3;
-                    }
-                }
-
-
-            if (wasLeft == 0) {
-                runningActions.add(new SequentialAction(
-                        new InstantAction(() -> verticalSlide.setPose(ConfigurationSecondRobot.bottom)),
-                        new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristIntake)),
-                        new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalOpen))
-                ));
-            }
-            if(wasLeft == 1){
-                runningActions.add(new SequentialAction(
-                        new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristWall)),
-                        new InstantAction(() -> verticalSlide.setPose(ConfigurationSecondRobot.bottom)),
-                        new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalClose))
-                ));
-            }
-            if (wasLeft == 2) {
-                runningActions.add(new SequentialAction(
-                        new InstantAction(() -> verticalSlide.setPose(ConfigurationSecondRobot.highBar)),
-                        new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristBar))
-                ));
-            }
-            if (wasLeft == 3) {
-                runningActions.add(new SequentialAction(
-                        new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristBasket)),
-                        new InstantAction(() -> verticalSlide.setPose(ConfigurationSecondRobot.topBasket))
-                ));
-            }
-            prevTimeLeft = currTime;
-            }
-        }
-
+        attachment.horizontalGrabber(gamepad1.right_trigger);
+        //vertical slide
+        attachment.verticalSlide(gamepad1.left_trigger, gamepad1.left_bumper);
         //transfer wrist
-        loopTimeCircle = currTime - prevTimeCircle;
-        //update input from gamepad
-        if (gamepad1.circle) {
-            if (loopTimeCircle <= desiredLoopms) {
-                if (wasCircle) {
-                    runningActions.add(new SequentialAction(
-                            new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristIntake))
-                    ));
-                } else {
-                    if (!wasCircle) {
-                        runningActions.add(new SequentialAction(
-                                new InstantAction(() -> verticalWrist.setPose(ConfigurationSecondRobot.verticalWristWall)),
-                                new InstantAction(() -> verticalGrabber.setPose(ConfigurationSecondRobot.verticalOpen))
-                        ));
-                    }
-                }
-                prevTimeCircle = currTime;
-                wasCircle = !wasCircle;
-            }
-        }
+        attachment.transferWrist(gamepad1.circle);
         //hook
-        loopTimeDPU = currTime - prevTimeDPU;
-        //update input from gamepad
-        if (gamepad1.dpad_up) {
-            if (loopTimeDPU >= desiredLoopms) {
-                if (wasDPU) {
-                    runningActions.add(new SequentialAction(
-                            new InstantAction(() -> verticalHanger.setPose(ConfigurationSecondRobot.verticalHangIn))
-                    ));
-                } else {
-                    if (!wasDPU) {
-                        runningActions.add(new SequentialAction(
-                                new InstantAction(() -> verticalHanger.setPose(ConfigurationSecondRobot.verticalHangOut))
-                        ));
-                    }
-                }
-                prevTimeDPU = currTime;
-                wasDPU = !wasDPU;
-            }
-        }
+        attachment.hook(gamepad1.dpad_up);
         //percise mode
-        loopTimeSquare = currTime - prevTimeSquare;
-        //update input from gamepad
-        if (gamepad1.square) {
-            if (loopTimeSquare >= desiredLoopms) {
-                prevTimeSquare = currTime;
-                wasSquare = !wasSquare;
-            }
-        }
-        //manual override for vertical slide
-        loopTimeCross = currTime - prevTimeCross;
-        //update input from gamepad
-        if (gamepad1.cross) {
-           if(loopTimeCross >= desiredLoopms){
-               if(wasCross){
-                   slidePose = verticalSlide.returnPose();
-                   if (gamepad1.left_stick_y != 0) {
-                       slidePose -= (int) (gamepad1.left_stick_y * 5);
-                       runningActions.add(new SequentialAction(
-                               new InstantAction(() -> verticalSlide.setPose(slidePose))
-                       ));
-                   }
-               }
-               prevTimeCross = currTime;
-               wasCross = !wasCross;
-           }
-        }
+        percise = attachment.percise(gamepad1.circle);
+       //manual slide override
+        attachment.verticalOverride(gamepad1.cross, gamepad1.dpad_up, gamepad1.dpad_down);
 
         //field centric robot control
         if(gamepad1.left_stick_x != 0 || gamepad1.left_stick_y != 0 || gamepad1.right_stick_x != 0 || gamepad1.right_bumper){
-            driveTrain.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.options, wasSquare, currentPose);
+            driveTrain.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.options, percise, currentPose);
         } else{
-            driveTrain.drive(0,0,0, false, wasSquare, currentPose);
+            driveTrain.drive(0,0,0, false, percise, currentPose);
         }
 
         //update runing actions
